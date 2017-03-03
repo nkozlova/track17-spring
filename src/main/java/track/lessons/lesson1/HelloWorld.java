6c33262aa8b0a2c6b35361f9c4da3fcc38e5bc2c
nkozlova <nadysha1997@yandex.ru>
2017-03-03 19:05:49 +0300

    Change packenge

--- src/main/java/track/lessons/lesson1/HelloWorld.java
+++ src/main/java/track/lessons/lesson1/HelloWorld.java
@@ -1,10 +1,9 @@
-package lessons.lesson1;
+package track.lessons.lesson1;
 
 /**
  *
  */
 public class HelloWorld {
-
     public static void main(String[] args) {
         System.out.println("Hello");
         for (int i = 0; i < args.length; i++) {
