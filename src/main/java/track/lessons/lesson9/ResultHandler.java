ac3ecb8f7936f0d7b938cb67c1c20964271e7f8b
nkozlova <nadysha1997@yandex.ru>
2017-04-13 13:41:08 +0300

    Delete junk files

--- src/main/java/track/lessons/lesson9/ResultHandler.java
+++
@@ -1,8 +0,0 @@
-package track.lessons.lesson9;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-public interface ResultHandler<T> {
-    T handle(ResultSet resultSet) throws SQLException;
-}
